ea5e24210d510f286c028bf9c126d547d61ba898
RektPunk <rektpunk@gmail.com>
2024-08-05 08:34:58 +0900

    feat - change error log

--- rektgbm/task.py
+++ rektgbm/task.py
@@ -25,33 +25,37 @@ SKLEARN_TASK_TYPE_MAPPER: Dict[SklearnTaskType, TaskType] = {
 }
 
 
-def _convert_target_to_list(column: Union[str, List[str]]) -> List[str]:
+def _convert_column_to_list(column: Union[str, List[str]]) -> List[str]:
     if isinstance(column, list) and len(column) != 1:
         raise ValueError(f"column must be str or list[str] of length 1")
     _column: List[str] = [column] if isinstance(column, str) else column
     return _column
 
 
-def _check_target_in_data(data: pd.DataFrame, column: Union[str, List[str]]) -> None:
+def _check_column_in_data(data: pd.DataFrame, column: Union[str, List[str]]) -> None:
     _dcols = data.columns
     _missing_cols = [col for col in column if col not in _dcols]
     if _missing_cols:
         raise ValueError(f"The target is missing")
 
 
-def _check_target(data: pd.DataFrame, column: Union[str, List[str]]) -> List[str]:
-    _column = _convert_target_to_list(column=column)
-    _check_target_in_data(data=data, column=column)
+def _check_column(data: pd.DataFrame, column: Union[str, List[str]]) -> List[str]:
+    _column = _convert_column_to_list(column=column)
+    _check_column_in_data(data=data, column=column)
     return _column
 
 
 def check_task_type(
-    data: pd.DataFrame, target: Union[str, List[str]], task_type: Optional[str]
+    data: pd.DataFrame,
+    target: Union[str, List[str]],
+    task_type: Optional[str] = None,
 ) -> TaskType:
-    _target: List[str] = _check_target(data=data, column=target)
+    _target: List[str] = _check_column(data=data, column=target)
     _type_inferred: str = type_of_target(data[_target].values)
     _sklearn_task_type = SklearnTaskType.get(_type_inferred)
     _task_type = SKLEARN_TASK_TYPE_MAPPER.get(_sklearn_task_type)
-    if _task_type != TaskType.get(task_type):
-        raise ValueError("Unable to infer 'task_type'.")
+    if task_type is not None and _task_type != TaskType.get(task_type):
+        raise ValueError(
+            f"Unable to infer 'task_type'. Expected '{TaskType.get(task_type).value}', but got '{_task_type.value}'."
+        )
     return _task_type
