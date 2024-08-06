21063098743ded382943b1e01bee3af74f833441
RektPunk <rektpunk@gmail.com>
2024-08-06 18:40:17 +0900

    add noqa to init

--- rektgbm/__init__.py
+++ rektgbm/__init__.py
@@ -1,5 +1,5 @@
-from rektgbm.dataset import RektDataset
-from rektgbm.optimize import RektOptimizer
-from rektgbm.rektgbm import RektGBM
+from rektgbm.dataset import RektDataset  # noqa
+from rektgbm.optimizer import RektOptimizer  # noqa
+from rektgbm.rektgbm import RektGBM  # noqa
 
 __version__ = "0.0.0"
