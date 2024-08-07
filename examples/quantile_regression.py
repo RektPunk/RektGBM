da810eaecd26c00f531236980fddce45ec8a8ba6
RektPunk <rektpunk@gmail.com>
2024-08-07 20:25:47 +0900

    update quantile regression

--- examples/quantile_regression.py
+++ examples/quantile_regression.py
@@ -1,33 +1,45 @@
+# Import necessary libraries from scikit-learn and rektgbm packages
 from sklearn.datasets import make_regression
 from sklearn.model_selection import train_test_split
 
 from rektgbm import RektDataset, RektGBM, RektOptimizer
 
+# Generate a synthetic regression dataset
 X, y = make_regression(n_samples=10_000, n_features=10, n_informative=5)
+
+# Split the dataset into training and testing sets
 X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size=0.1, random_state=42
 )
+
+# Create RektDataset objects for training and testing data
 dtrain = RektDataset(data=X_train, label=y_train)
 dtest = RektDataset(data=X_test, label=y_test)
 
-
+# Initialize RektOptimizer with specified settings for regression task
 rekt_optimizer = RektOptimizer(
     method="both",  # Optimization method: options are both, lightgbm, xgboost
     task_type="regression",  # Type of task: regression
     objective="quantile",  # Objective function
     additional_params={
-        "alpha": 0.5,  # # Additional parameter for quanrile; "quantile_alpha" can also be used
+        "alpha": 0.5,  # Additional parameter for quantile; "quantile_alpha" can also be used
     },
 )
 
+# Optimize hyperparameters using the training dataset over a specified number of trials
 rekt_optimizer.optimize_params(
-    dataset=RektDataset(X_train, y_train),
+    dataset=dtrain,
     n_trials=10,
 )
+
+# Print the best hyperparameters found during optimization
 print(rekt_optimizer.best_params)
 
+# Initialize RektGBM model with the best hyperparameters
 rekt_gbm = RektGBM(**rekt_optimizer.best_params)
-rekt_gbm.fit(
-    dataset=RektDataset(X_train, y_train),
-)
-preds = rekt_gbm.predict(RektDataset(X_test, y_train))
+
+# Train the model using the training dataset
+rekt_gbm.fit(dataset=dtrain)
+
+# Predict on the test dataset using the trained model
+preds = rekt_gbm.predict(dataset=dtest)
